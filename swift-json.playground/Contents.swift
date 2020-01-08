66016b001bc98ea402b49b48b3040223f072f64e
Edward Chung <edward.chung@redfin.com>
2020-01-08 12:45:01 -0800

    encode example

--- swift-json.playground/Contents.swift
+++ swift-json.playground/Contents.swift
@@ -1,38 +1,17 @@
-import PlaygroundSupport
 import UIKit
-PlaygroundPage.current.needsIndefiniteExecution = true
 
 struct Todo: Codable {
-    let userNumber : Int
-    let todoId : Int
-    let todoTitle : String
-    let isCompleted : Bool
-
-  enum CodingKeys: String, CodingKey {
-      case userNumber = "userId"
-      case todoId = "id"
-      case todoTitle = "title"
-      case isCompleted = "completed"
-  }
+    let userId : Int
+    let id : Int
+    let title : String
+    let completed : Bool
 }
 
-func getTodo() {
-    // Check URL is valid
-    guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1") else { return }
-    URLSession.shared.dataTask(with: url) { (data, response, error) in
-        // Check data is not nil
-        guard let data = data else { return }
+let myTodo = Todo(userId: 1, id: 1, title: "Finish json swift tutorial", completed: false)
 
-        do {
-            let todo = try JSONDecoder().decode(Todo.self, from: data)
-            print(todo.userNumber)
-            print(todo.todoId)
-            print(todo.todoTitle)
-            print(todo.isCompleted)
-        } catch {
-            print("error!")
-        }
-    }.resume()
-}
+let encoder = JSONEncoder()
+// Output formatting is optional, just so it looks good in console
+encoder.outputFormatting = .prettyPrinted
 
-getTodo()
+let data = try encoder.encode(myTodo)
+print(String(data: data, encoding: .utf8)!)
